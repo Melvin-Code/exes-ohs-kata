2055cb0800d23d4fde23e4a10edc271e894f9612
salamander <melvingonzalez470@gmail.com>
2019-12-30 20:58:29 -0500

    xo

---
+++ exes-ohs.js
@@ -0,0 +1,15 @@
+function XO(str) {
+  same = str.toLowerCase()
+  var x = 0
+  var o = 0
+  for (var i=0; i<same.length; i++){
+    if(same[i] === 'x'){
+      x ++
+    } else if(same[i] === 'o'){
+      o ++
+    } 
+  }
+  return x === o
+}
+
+XO('zzoo')
